bffbd840b7054976e21b8c51cde8ef39939c737f
anthony8912310 <73423154+anthony8912310@users.noreply.github.com>
2021-05-07 11:48:56 +0800

    Update and rename test.c to pme01.c

--- pme01.c
+++ pme01.c
@@ -7,21 +7,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-void Permutation(int m, int n, int arr[], int temp[], char result[]);
-void find(int n, char result[]);
-void Permutation(int m, int n, int arr[], int temp[], char result[])
+void Permutation(int m, int n, int arr[], int temp[], int sum[]);
+void find(int n, char result[], int sum[]);
+void Permutation(int m, int n, int arr[], int temp[], int sum[])
 {
     int i;
-    char tmp;
+    char tmp[n];
     if(m >= n)
     {
         for (i = 0; i <= n-1; i++)
         {
-            tmp = arr[i]+'0';
-            result[i] = tmp;
-            printf("%d", arr[i]);
+            sprintf(&tmp[i] , "%d", arr[i]);
         }
-        printf("\n");
+//        printf("%s\n", tmp);
+        find(n, tmp, sum);
     }
     else
     {
@@ -31,19 +30,18 @@ void Permutation(int m, int n, int arr[], int temp[], char result[])
             {
                 temp[i] = 1;
                 arr[m] = i + 1;
-                Permutation(m + 1, n, arr, temp, result);
+                Permutation(m + 1, n, arr, temp, sum);
                 temp[i] = 0;
             }
         }
     }
 }
-void find(int n, char result[])
+void find(int n, char result[], int sum[])
 {
-//    printf("%s\n",result);
     for (int r=1; r<= n; r++) {
-        printf("%d\n",r);
+        
         if (r==1 && result[0]=='1') {
-            printf("ok\n");
+            sum[r-1]+=1;
             continue;
         }
         int min;
@@ -56,11 +54,10 @@ void find(int n, char result[])
                 min=result[i];
             }
         }
-        
         for (int j=r-1; j<n; j++) {
             if (result[j]<min) {
                 if (result[j]=='1') {
-                    printf("ok\n");
+                    sum[r-1]+=1;
                     break;
                 }else
                 {
@@ -73,11 +70,20 @@ void find(int n, char result[])
 int main(int argc, const char * argv[]) {
     int arr[15], temp[15], n;
     scanf("%d", &n);
-    int i;
+    int i, k=1;
     for (i = 0; i < n; i++)
         temp[i] = 0;
-
-    char result[n];
-    Permutation(0, n, arr, temp, result);
+    int sum[n];
+    float ans;
+    for (int i = 0; i < n; i++)
+    {
+        sum[i]=0;
+        k*=i+1;
+    }
+    Permutation(0, n, arr, temp, sum);
+    for (int i = 0; i < n; i++) {
+        ans = (float) sum[i] / (float) k;
+        printf("%d:%.3f\n", i+1, ans);
+    }
     return 0;
 }
